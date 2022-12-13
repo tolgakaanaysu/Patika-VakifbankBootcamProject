5117e073cdc097b96b6fbe5d70e9c2973a2b661d
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-12-13 03:22:13 +0300

    create HomepageViewController

---
+++ BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/View/HomepageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomepageViewController.swift
+//  BootcampFinalProject
+//
+//  Created by Tolga Kağan Aysu on 13.12.2022.
+//
+
+import UIKit
+
+protocol HomepageViewControllerDelegate: AnyObject {
+    
+}
+
+final class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
