c9762bd3b8ff3a3fd6a58f63d1261db957fd7448
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-12-13 07:54:12 +0300

    added CollectionViewDataSource methods

--- BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/ViewModel/HomepageViewModel.swift
+++ BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/ViewModel/HomepageViewModel.swift
@@ -11,17 +11,26 @@ protocol HomepageViewModelDelegate {
     var view: HomepageViewControllerDelegate? { get set}
     
     func viewDidLoad()
+    func numberOfItemsInSection() -> Int
+    func cellForItemAt(at indexPath: IndexPath) -> Game?
 }
-
 
 final class HomepageViewModel: HomepageViewModelDelegate {
     //MARK: - Property
     weak var view: HomepageViewControllerDelegate?
-    
+    var gameList = [Game]()
     
     //MARK: - Lifecycle
     func viewDidLoad() {
-        
+        view?.prepareCollectionView()
     }
     
+    //MARK: - CollectionViewDataSourceMethods
+    func numberOfItemsInSection() -> Int {
+        gameList.count
+    }
+    
+    func cellForItemAt(at indexPath: IndexPath) -> Game? {
+        gameList[indexPath.row]
+    }
 }
