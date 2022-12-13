cc2e2d4b4311ea47850bbf4a46b23e2e18dc15ab
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-12-13 06:31:56 +0300

    added property and coding key

--- BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/Model/GameModel.swift
+++ BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/Model/GameModel.swift
@@ -7,7 +7,20 @@
 
 import Foundation
 
-final class Game: Codable {
-   
+struct Game: Decodable {
+    let id: Int
+    let name: String
+    let nameOriginal:  String
+    let gameDescription:  String
+    let descriptionRaw:  String
+    let rating : Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, name
+        case nameOriginal = "name_original"
+        case gameDescription = "description"
+        case rating
+        case descriptionRaw = "description_raw"
+    }
 }
 
