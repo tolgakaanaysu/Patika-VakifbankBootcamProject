cf343208f97f1a531d1d44955d9bafe29c3a01ed
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-12-13 07:55:00 +0300

    added collectionview and datasource methods

--- BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/View/HomepageViewController.swift
+++ BootcampFinalProject/BootcampFinalProject/Feature/Homepage-module/View/HomepageViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 protocol HomepageViewControllerDelegate: AnyObject {
-    
+    func prepareCollectionView()
 }
 
 final class HomepageViewController: UIViewController {
+    //MARK: - IBOutlet
+    @IBOutlet private weak var gameCollectionView: UICollectionView!
+    
     //MARK: - Property
     private lazy var viewModel: HomepageViewModelDelegate = HomepageViewModel()
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +26,28 @@ final class HomepageViewController: UIViewController {
     }
 }
 
-extension HomepageViewController: HomepageViewControllerDelegate {
+//MARK: - CollectionViewDataSource
+extension HomepageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.numberOfItemsInSection()
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GameCollectionViewCell.identifier ,for: indexPath) as? GameCollectionViewCell else {
+            print("xib not found")
+            return .init()
+        }
+        guard let game = viewModel.cellForItemAt(at: indexPath) else { return .init() }
+        
+        cell.configureCell(with: game)
+        return cell
+    }
+}
+
+//MARK: - HomepageViewControllerDelegate
+extension HomepageViewController: HomepageViewControllerDelegate {
+    func prepareCollectionView() {
+        gameCollectionView.dataSource = self
+        gameCollectionView.register(GameCollectionViewCell.nib, forCellWithReuseIdentifier: GameCollectionViewCell.identifier)
+    }
 }
