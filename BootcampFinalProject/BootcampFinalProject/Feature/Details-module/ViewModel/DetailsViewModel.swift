c0f11ca1238b54a1505fc5532e51a902c001120a
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-12-14 15:02:59 +0300

    added favoriteButtonClicked method and save-delete favori game

--- BootcampFinalProject/BootcampFinalProject/Feature/Details-module/ViewModel/DetailsViewModel.swift
+++ BootcampFinalProject/BootcampFinalProject/Feature/Details-module/ViewModel/DetailsViewModel.swift
@@ -9,17 +9,49 @@ import Foundation
 
 protocol DetailsViewModelDelegate {
     var view: DetailsViewControllerDelegate? { get set }
-    
-    func viewDidLoad(id: Int)
+    var id: Int! { get set }
+    func viewDidLoad()
+    func favoriteButtonClicked()
 }
 
 final class DetailsViewModel: DetailsViewModelDelegate {
     //MARK: - Property
     var view: DetailsViewControllerDelegate?
+    var id: Int!
+    private var gameIsFavorite: Bool = false
     
     //MARK: - Lifecycle
-    func viewDidLoad(id: Int) {
+    func viewDidLoad() {
         getGameDetails(by: id)
+        getGameIsFavorite()
+    }
+    
+    // Methods
+    func favoriteButtonClicked() {
+        
+        if !gameIsFavorite {
+            CoreDataFavoriGameClient.shared.saveFavoriteGame(id: id.toString()){ [weak self] result in
+                switch result {
+                case .success(let success):
+                    self?.view?.showSuccessMessage(message: success.message)
+                case .failure(let failure):
+                    self?.view?.showErrorMessage(message: failure.message)
+                    return
+                }
+            }
+        } else {
+            CoreDataFavoriGameClient.shared.deleteFavoriGame(id: id.toString()){ [weak self] result in
+                switch result {
+                case .success(let success):
+                    self?.view?.showSuccessMessage(message: success.message)
+                case .failure(let failure):
+                    self?.view?.showErrorMessage(message: failure.message)
+                    return
+                }
+            }
+        }
+        gameIsFavorite.toggle()
+        view?.changeButtonColor(gameIsFavorite)
     }
     
     //MARK: - Private Methods
@@ -32,5 +64,15 @@ final class DetailsViewModel: DetailsViewModelDelegate {
                 self?.view?.prepareInterfaceComponent(game: game)
             }
         }
+    }
+    
+    private func getGameIsFavorite(){
+        guard let _ = CoreDataFavoriGameClient.shared.getFavoriteGame(by: id.toString()) else {
+            self.gameIsFavorite = false
+            view?.changeButtonColor(false)
+            return
+        }
+        gameIsFavorite = true
+        view?.changeButtonColor(true)
     }
 }
