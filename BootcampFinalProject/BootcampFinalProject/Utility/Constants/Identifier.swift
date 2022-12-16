0fc596c052199f3324af532ed83889f31853e235
Tolga Kağan Aysu <tolga.kagan@hotmail.com>
2022-12-17 00:27:13 +0300

    edited file path

---
+++ BootcampFinalProject/BootcampFinalProject/Utility/Constants/Identifier.swift
@@ -0,0 +1,13 @@
+//
+//  Identifier.swift
+//  BootcampFinalProject
+//
+//  Created by Tolga Kağan Aysu on 16.12.2022.
+//
+
+import Foundation
+
+struct Identifiers {
+    static let homepageVCToDetailsVC = "homepageToDetail"
+    static let favoriteListVCToDetailsVC = "favoriteListToDetail"
+}
